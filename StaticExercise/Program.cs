c5213115eee27473d8a2966f8aa36532511ec427
Jeffrey Jose <jefjosecool@gmail.com>
2022-11-02 10:13:28 -0400

    "Finished"

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace StaticExercise
+﻿//JESUS
+namespace StaticExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+
+
+                Console.WriteLine("Would you like to convert Farheinheit or celsius");
+
+                var UserInput = Console.ReadLine();
+               // while (UserInput != "Farheinheit" || UserInput != "Celsius")
+           
+                if (UserInput == "Farheinheit")
+                {
+                    Console.WriteLine("Please input fareinheight amount");
+                    var input = double.Parse(Console.ReadLine());
+
+                    var total = TempConverter.FahrenheitToCelsius(input);
+                    Console.WriteLine(total);
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Please enter celsius amount");
+
+                    var celsiusamount = double.Parse(Console.ReadLine());
+
+                    var total = TempConverter.CelsiusToFahrenheit(celsiusamount);
+                    Console.WriteLine(total);
+                }
+
+
+
+
+
+
+
+
+
+            }
     }
-}
+    }
+
+
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+
+        public static double FahrenheitToCelsius(double answer)
+        {
+            return (answer - 32) * .5556;
+
+        }
+
+
+        public static double CelsiusToFahrenheit(double input)
+        {
+            return (input * 1.8) + 32;
+
+        }
+
+
+
+
+    }
+}
